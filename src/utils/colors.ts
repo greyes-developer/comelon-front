7a46fa267b662c26f2629272d5703abb2e0c99aa
rogerpervaz <roger@Salvadors-Mac-mini.local>
2023-06-27 18:45:32 -0600

    colores

---
+++ src/utils/colors.ts
@@ -0,0 +1,17 @@
+const neutralColor = '#0E0E10'
+const primaryColor = '#FFFF';
+const secondaryColor = '#FF7C5A';
+const yellowColor = '#FFBF5C';
+const greenColor = '#41E8CA';
+const purpleColor = '#D86AEA';
+const redColor = '#F25B5B';
+
+export default {
+    neutralColor,
+    primaryColor,
+    secondaryColor,
+    yellowColor,
+    greenColor,
+    purpleColor,
+    redColor
+}
